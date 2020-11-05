a2e5e33a14cc9959b97f6586fb81e4de45c694b1
Bruno Aguiar <brunoaguiar@Brunos-MBP.Home>
2020-11-05 17:41:28 +0000

    ola mundo

---
+++ app/validate.py
@@ -0,0 +1,8 @@
+from lxml import etree
+schemadoc = etree.parse("top_artists.xsd")
+schema = etree.XMLSchema(schemadoc)
+parser = etree.XMLParser(schema=schema)
+tree = etree.parse("top_artists.xml")
+schema.assertValid(tree)
+
+
